3c0e4bcdd3731c272fc70404ce0cb096ea7ef60e
firen777 <hivemindac1995@gmail.com>
2018-01-09 16:57:31 +0800

    file_lab 2, gitignore update

--- file_lab.py
+++ file_lab.py
@@ -6,3 +6,29 @@ fw.write("Hello World!\n")
 fw.write("Hello World!\n")
 fw.close()
 
+"""
+'r'	open for reading (default)
+'w'	open for writing, truncating the file first
+'x'	open for exclusive creation, failing if the file already exists
+'a'	open for writing, appending to the end of the file if it exists
+'b'	binary mode
+'t'	text mode (default)
+'+'	open a disk file for updating (reading and writing)
+'U'	universal newlines mode (deprecated)
+"""
+
+# fr = open('./file_lab.txt', 'r')
+# buff = fr.readline()
+# print(buff[:-1])
+# buff = fr.readline()
+# print(buff[:-1])
+# buff = fr.readline()
+# print(buff[:-1])
+# buff = fr.readline()
+# print(buff[:-1])
+# fr.close
+
+with open('./file_lab.txt') as fr:  #with statement: syntactic sugar for try/finally block. cleans stuff up
+    for l in fr:    #file object is line iterable
+        print(l[:-1])    #[:-1] means list[0.. second last]
+
