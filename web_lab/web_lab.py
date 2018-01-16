dbb60947180dc2863f72238d36679c4dcfdd4888
firen777 <hivemindac1995@gmail.com>
2018-01-16 19:39:24 +0800

    web_lab commit whatever

--- web_lab/web_lab.py
+++ web_lab/web_lab.py
@@ -1,14 +1,27 @@
+"""
+required module: beautifulsoup4
+"""
+
 from urllib import request
+from bs4 import BeautifulSoup
 
-# stock photo meme: https://pbs.twimg.com/profile_images/676496806646272000/k2hLF7zz.jpg
+def getmeme_jpg():
+    """function to get a meme jpg at [here](https://pbs.twimg.com/profile_images/676496806646272000/k2hLF7zz.jpg)
 
-pic_url = 'https://pbs.twimg.com/profile_images/676496806646272000/k2hLF7zz.jpg' 
-#if "<urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] ... " error for https url:
-#go to /Application/Python 3.6/
-#Install Certificates.command
-#credit to: https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify
+    demonstrating basic request and file writing
+    """
 
-response = request.urlopen(pic_url)
+    # stock photo meme: https://pbs.twimg.com/profile_images/676496806646272000/k2hLF7zz.jpg
 
-with open('./meme.jpg', 'wb') as fw:
-    fw.write(response.read())
+    pic_url = 'https://pbs.twimg.com/profile_images/676496806646272000/k2hLF7zz.jpg' 
+    #if "<urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] ... " error for HTTPS:
+    #go to /Application/Python 3.6/
+    #Install Certificates.command
+    #credit to: https://stackoverflow.com/questions/42098126/mac-osx-python-ssl-sslerror-ssl-certificate-verify-failed-certificate-verify
+
+    response = request.urlopen(pic_url)
+
+    with open('./meme.jpg', 'wb') as fw:
+        fw.write(response.read())
+
+
