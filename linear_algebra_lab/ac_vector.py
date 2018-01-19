5c7c51c8e075ce126d9d6691d07530351436c8a0
Albert Chan <hivemindac1995@gmail.com>
2018-01-20 01:34:38 +0800

    linear algebra first commit

---
+++ linear_algebra_lab/ac_vector.py
@@ -0,0 +1,9 @@
+"""
+Review my Linear Algebra and play with OO in Python in the process.
+
+[Underscore and stuffs](https://stackoverflow.com/questions/1301346/what-is-the-meaning-of-a-single-and-a-double-underscore-before-an-object-name)
+['Private' field](https://stackoverflow.com/questions/2064202/private-members-in-python)
+"""
+
+class AC_2DVector:
+    
