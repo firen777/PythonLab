f61a9bc8aacf6b17a95ba75c5cb6bafe0cf7b38e
firen777 <hivemindac1995@gmail.com>
2018-01-24 09:34:49 -0800

    rename to matrix. Class def

--- linear_algebra_lab/ac_matrix.py
+++ linear_algebra_lab/ac_matrix.py
@@ -1,12 +1,26 @@
 """
-Review my Linear Algebra and play with OO in Python in the process.
+Reviewing Linear Algebra and play with OO in Python in the process.
 
 [Underscore and stuffs](https://stackoverflow.com/questions/1301346/what-is-the-meaning-of-a-single-and-a-double-underscore-before-an-object-name)
 ['Private' field](https://stackoverflow.com/questions/2064202/private-members-in-python)
 """
 import sys
 
-#class AC_2DVector:
+class AC_Matrix:
+    def __init__(self, mat=[[1,0,0],[0,1,0],[0,0,1]]):
+        self.mat = mat
+
+    def __str__(self):
+        # str_result = "["
+        # for v in self.mat:
+        #     str_result = str_result + "["
+        #     for n in v:
+        #         str_result = str_result + str(n) + ","
+        #     str_result = str_result[:-1] + "],"
+        # str_result = str_result[:-1] + "]"
+        # return str_result
+        return str(self.mat)
+
 
 def trans_factory(mat=[[1,0,0],[0,1,0],[0,0,1]]):
     """
@@ -29,7 +43,9 @@ def main(argv):
     print(mat1[0][0])
     test_func = trans_factory()
     test_func(mat1)
-    pass
+
+    mat1 = AC_Matrix(mat = [[1,0,0],[0,2.9,300]])
+    print(mat1)
 
 
 if __name__ == "__main__":
